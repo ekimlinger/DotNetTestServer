fa5582055c2435e060e31a8a7348684b2bb2e2ef
ekimlinger <ekimlinger@gmail.com>
2016-11-16 22:51:59 -0600

    Initial upload for Test Server

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+
+//  namespace ConsoleApplication
+//{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            // app.UseStaticFiles();
+            
+            app.Run(  context => 
+            { 
+                return context.Response.WriteAsync("Hello World on the Web!");
+            });
+
+
+        }
+
+    }
+
+    public class Program
+    {                    
+        public static void Main(string[] args)
+        {
+             var host = new WebHostBuilder()
+                    .UseKestrel()
+                    .UseStartup<Startup>()
+                    .Build();
+
+            
+
+            host.Run();
+        }
+    }
+// }
